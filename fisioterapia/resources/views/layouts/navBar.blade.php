3c087e355f02303a44444968ec090ea6e4eaa6c5
Takeshi <g.t.n.ramirez@gmail.com>
2019-03-12 14:24:20 -0600

    Fix navbar materialize

--- fisioterapia/resources/views/layouts/navBar.blade.php
+++ fisioterapia/resources/views/layouts/navBar.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.app')
 @section('stylesheet')
 <link href="{{{ asset('css/style_navbar.css') }}}" rel="stylesheet">
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+
+
+
+
+
 
 
 @endsection
@@ -9,70 +21,30 @@
 
 @section('content')
 
+<nav>
+  <div id="nav" class="nav-wrapper">
 
-<div class="d-flex toggled" id="wrapper">
+    <a href="#" data-target="mobile-demo" class="sidenav-trigger show-on-small show-on-medium-and-up">
+      <i class="material-icons">menu</i>
+    </a>
 
-    <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading">Start Bootstrap </div>
-      <div class="list-group list-group-flush">
-        <a href="#" class="list-group-item list-group-item-action ">Dashboard</a>
-        <a href="#" class="list-group-item list-group-item-action ">Shortcuts</a>
-        <a href="#" class="list-group-item list-group-item-action ">Overview</a>
-        <a href="#" class="list-group-item list-group-item-action ">Events</a>
-        <a href="#" class="list-group-item list-group-item-action ">Profile</a>
-        <a href="#" class="list-group-item list-group-item-action ">Status</a>
-      </div>
-    </div>
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-
-      <nav id="barra" class="navbar navbar-expand-lg navbar-light bg-light">
-
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <button class="btn btn-primary" id="menu-toggle">
-            <a href="#" id="menu_on">
-  	          <span></span>
-  	          <span></span>
-  	          <span></span>
-            </a>
-          </button>
-
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="iconoNavbar">
-            <img src="{{{ asset('img/logo/logo_horizontal_blanco.png') }}}" id="icon" alt="User Icon" />
-          </div>
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Cerrar sesión</a>
-            </li>
-
-
-          </ul>
+      <div id="logo" class="row">
+        <div class="col s6">
+          <img id="img_logo" class="resposive-img" src="../img/logo/logo_horizontal_blanco.png">
         </div>
-      </nav>
+      </div>
 
-
-    <!-- /#page-content-wrapper -->
 
   </div>
-  <!-- /#wrapper -->
+</nav>
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="mobile.html">Mobile</a></li>
+  </ul>
 
-  <!-- Bootstrap core JavaScript -->
-
-  <!-- Menu Toggle Script -->
-  <script>
-    $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-    });
-  </script>
-
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script src="{{{ asset('js/init.js') }}}"></script>
 
 @endsection
