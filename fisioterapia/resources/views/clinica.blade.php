342e55e6da144fbd1fdcac1604b5d373d3d6c106
Takeshi <g.t.n.ramirez@gmail.com>
2019-03-13 22:35:23 -0600

    Rresponsividad en login e historia clinica

--- fisioterapia/resources/views/clinica.blade.php
+++ fisioterapia/resources/views/clinica.blade.php
@@ -8,26 +8,38 @@
       </div>
     </div>
   </div>
-  <div class="row justify-content-md-center">
-    <div class="col-md-auto">
-      padecimiento
+  <div class="justify-content-md-center">
+    <div class="row">
+      <div class="col-md-2">Padecimiento</div>
+      <div class="col-md-2">Sí/No</div>
+      <div class="col-md-2">¿Quién?</div>
+      <div class="col-md-2">¿Cuál padecimiento?</div>
     </div>
-    <div class="col-md-auto">
-      <p>
+    <div class="row">
+      <div class="col-md-2">Enfermedades reumatológicas</div>
+      <div class="col-md-2">
+        <p>
         <label>
           <input name="yourchoice" type="radio" checked />
           <span>Yes</span>
         </label>
-      </p>
-      <p>
-        <label>
-          <input name="yourchoice" type="radio" />
-          <span>No</span>
-        </label>
-      </p>
-    </div>
-    <div class="col-md-auto">
-      ¿quien?
+        </p>
+        <p>
+          <label>
+            <input name="yourchoice" type="radio" />
+            <span>No</span>
+          </label>
+        </p>
+      </div>
+      <div class="col-md-2">
+        <select>
+          <option>Madre</option>
+          <option>Padre</option>
+          <option>Hermanos</option>
+          <option>Abuelos</option>
+          <option>otros</option>
+        </select>
+      </div>
     </div>
   </div>
 @endsection
