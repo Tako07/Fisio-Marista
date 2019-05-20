e63c9f717963025462d29403820486192b80cdbc
Takeshi <g.t.n.ramirez@gmail.com>
2019-05-19 20:29:27 -0500

    Excel sólo con pacientes

--- marista/app/Exports/PacientesExport.php
+++ marista/app/Exports/PacientesExport.php
@@ -5,23 +5,44 @@ namespace App\Exports;
 use App\Models\paciente;
 use App\Models\historia_clinica;
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class PacientesExport implements FromArray
+
+class PacientesExport implements FromCollection, WithHeadings
 {
 
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function array():array
+    public function collection()
     {
-      $pacientes=paciente::join('historia_clinica as h', 'paciente.id_paciente','=','h.id_paciente')->get();
+      //$pacientes=
+      return paciente::all();
+      //join('historia_clinica as h', 'paciente.id_paciente','=','h.id_paciente')->get();
       //dd($pacientes);
-      $arreglo=array();
+      /*$arreglo=array();
       foreach ($pacientes as $paciente ) {
         array_push($arreglo,json_decode($paciente->ant_heredo_fam,true));
         array_push($arreglo,json_decode($paciente->ant_pers_no_pat,true));
         array_push($arreglo,json_decode($paciente->ant_pers_pat,true));
-      }
-        return $arreglo;
+      }*/
+        //return $arreglo;
     }
+    public function headings(): array
+    {
+        return [
+            'Nombres',
+            'Apellido paterno',
+            'Apellido materno',
+            'Edad',
+            'CURP',
+            'Sexo',
+            'Nacionalidad',
+            'Estado civil',
+            'Profeción'
+
+        ];
+    }
+
 }
